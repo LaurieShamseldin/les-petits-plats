131b570c926ff404742585043b75940e63d47005
Laurie Shamseldin <laurie.musson@gmail.com>
2023-10-23 17:06:30 +0200

    Add close search

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -3,8 +3,14 @@ import recipeFactory from '../factories/recipe.js';
 
 
 const displayRecipes = (recipesData) => {
-  console.log(recipesData);
-  
+  console.log(typeof recipesData);
+  const recipeSection = document.getElementById("recips");
+  recipesData.forEach((recipe) => {
+    const recipModel = recipeFactory(recipe);
+    const recipesPageDOM = recipModel.getRecipeCardDOM();
+    recipeSection.appendChild(recipesPageDOM);
+  });
+
 }
 
 
